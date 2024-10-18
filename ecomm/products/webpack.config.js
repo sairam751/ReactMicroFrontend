bc0f9318b08873e9b8cff04cabe1e8c68ed1fbc3
sairam751 <sairam751348@gmail.com>
2024-10-18 14:11:10 +0530

    To config packages only once(If we have diff versions in diff apps)_It will show warking in console if two diff versions are loaded

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
         exposes: {
           './ProductsIndex': './src/index',
         },
-        shared: ['faker'],
+        //shared: ['faker'],
+      shared:{
+        faker:{
+            singleton: true,
+        }
+      }
       }),
     new HtmlWebpackPlugin({
       template: './public/index.html',

0f7ae7586a92cf00973b3c10ea919a83ea37bae8
sairam751 <sairam751348@gmail.com>
2024-10-18 13:16:32 +0530

    ConfigurationForSharedModules(Will Not Wotk Independently as Faker is loaded async)

--- ecomm/Cart/webpack.config.js
+++ ecomm/Cart/webpack.config.js
@@ -8,11 +8,12 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'Cart',
+      name: 'cart',
       filename: 'remoteEntry.js',
       exposes: {
         './CartShow': './src/index',
       },
+      shared: ['faker'],
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
